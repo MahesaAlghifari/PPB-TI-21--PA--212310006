75cba393940715b82f5c94258df71044b333577d
MahesaAlghifari <212310006@student.ibik.ac.id>
2023-05-01 03:26:03 +0700

    212310006

---
+++ Pertemuan2_212310006/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+// import Bicycle from "src/components/Bicycle";
+// import Car from "src/components/Car";
+// import Motorcycle from "src/components/Motorcycle";
+// import Forms from "src/components/Forms";
+// import kis from "src/components/kis";
+import No1 from "./src/components/No1";
+import No2 from "./src/components/No2";
+import No3 from "./src/components/No3";
+import No4RFC from "./src/components/No4RFC";
+import No4RCC from "./src/components/No4RCC";
+
+const App = () => {
+  return <No4RCC />;
+};
+
+export default App;
